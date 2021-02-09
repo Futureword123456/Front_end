3b22cc46cc0ba1c68cc6fa7e602eabd4461eef3e
Administrator <2635681517@qq.com>
2021-02-09 17:54:34 +0800

    html+css+javascript

---
+++ js/js2.js
@@ -0,0 +1,26 @@
+var flag = true;
+function checkInterest() {
+    var  interest=document.getElementsByName("interest");
+    for (var i = 0; i <interest.length ; i++) {
+        interest[i].checked = flag;
+    }
+
+    if(flag===true){
+        document.getElementById("btn1").value="全选";
+
+    }else {
+        document.getElementById("btn1").value="全不选";
+
+    }
+    flag=!flag;//开关变量
+}
+
+function checkInterest1() {
+    var  interest=document.getElementsByName("interest");
+    for (var i = 0; i <interest.length ; i++) {
+        interest[i].checked =!interest[i].checked;
+        console.log(interest[i].value);
+    }
+}
+
+
